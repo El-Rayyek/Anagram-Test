48154f5cadc9cda2829c50f2d9dac932e7ef1456
El-Rayyek <mohamed.abdelfattah439@eng.s-mu.edu.eg>
2022-10-03 17:38:12 +0200

    use dictionary

--- test anagram.py
+++ test anagram.py
@@ -1,23 +1,36 @@
 
-
 def test_anagram(word1,word2):
-    count = 0
+    word1 = list(word1)
+    word2 = list(word2)
+    dict1 = dict()
+    dict2 = dict()
     for i in range(len(word1)):
-        for j in range(len(word2)):
-            if word1[i] == word2[j]:
-                count += 1
-                word2.replace(word2[j],'')
+        if word1[i] == " ":
+            continue
+        dict1[word1[i]] = dict1.get(word1[i],0) +1
+        dict2[word2[i]] = dict2.get(word2[i],0) +1
+    flag = False
+    for j in dict1.keys():
+        if j in dict2:
+            if dict1[j] == dict2[j]:
+                flag = True
+            else:
+                flag = False
                 break
-            
-    if count == len(word1):
-        return True
-    else:
-        return False
 
-word1 = input("Enter First word :")
-word2 = input("Enter second word :")
-
-print(test_anagram(word1,word2))
-                
+    return flag
         
 
+while True:
+        
+    word1 = input("Enter First word :")
+    word2 = input("Enter Second word :")
+
+    if len(word1) != len(word2):
+        print("Please enter the same count of letters at two words")
+        continue
+    
+    print(f"The anagram test is {test_anagram(word1,word2)}")
+    quest = input("Do you want another one? (y/n)")
+    if quest.lower() == 'n':
+        break
